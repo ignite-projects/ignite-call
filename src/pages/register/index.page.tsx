040815cf94ce3df3591b3f21baedefb40980cc84
Robson Natanael <natanaelrobson@gmail.com>
2023-11-04 15:02:45 -0300

    feat: created user validation

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -7,6 +7,7 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 import { api } from '../../lib/axios'
+import { AxiosError } from 'axios'
 
 const registerFormSchema = z.object({
   username: z
@@ -46,6 +47,11 @@ export default function Register() {
         username: data.username,
       })
     } catch (error) {
+      if (error instanceof AxiosError && error?.response?.data?.message) {
+        alert(error.response.data.message)
+        return
+      }
+
       console.log(error)
     }
   }
